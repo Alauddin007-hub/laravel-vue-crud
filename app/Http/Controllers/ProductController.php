c18e1fe3c9c6ee2eb4e1dce62fb4b155b9ac26db
Alauddin007-hub <mdalauddinalsafin@gmail.com>
2024-03-03 09:30:47 +0600

    first commit

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        $products = Product::all()->toArray();
+        $ddd = array_reverse($products);
+        // dd($ddd);
+        return array_reverse($products);      
+    }
+    public function store(Request $request)
+    {
+        $product = new Product([
+            'name' => $request->input('name'),
+            'detail' => $request->input('detail')
+        ]);
+        $product->save();
+        return response()->json('Product created!');
+    }
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+    public function update($id, Request $request)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json('Product updated!');
+    }
+    public function destroy($id)
+    {
+        $product = Product::find($id);
+        $product->delete();
+        return response()->json('Product deleted!');
+    }
+}
